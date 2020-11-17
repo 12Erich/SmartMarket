dd8075898c10bac9bd0ee025ffef7f0870872fea
Erich <erik.fernandes@itlab.com>
2020-11-16 22:01:45 -0300

    Definição de layout da tela principal

--- SmartMarket/SmartMarket/MainWindow.xaml.cs
+++ SmartMarket/SmartMarket/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -32,14 +33,38 @@ namespace SmartMarket
 
         private void btnCloseMenu_Click(object sender, RoutedEventArgs e)
         {
-            btnOpenMenu.Visibility = Visibility.Visible;
-            btnCloseMenu.Visibility = Visibility.Collapsed;
+            //btnOpenMenu.Visibility = Visibility.Visible;
+            //btnCloseMenu.Visibility = Visibility.Collapsed;
         }
 
         private void btnOpenMenu_Click(object sender, RoutedEventArgs e)
         {
-            btnCloseMenu.Visibility = Visibility.Visible;
-            btnOpenMenu.Visibility = Visibility.Collapsed;
+            //btnCloseMenu.Visibility = Visibility.Visible;
+            //btnOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        bool MenuClosed = false;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuClosed)
+            {
+                Storyboard openMenu = (Storyboard)button.FindResource("OpenMenu");
+                openMenu.Begin();
+            }
+            else
+            {
+               Storyboard closeMenu = (Storyboard)button.FindResource("CloseMenu");
+               closeMenu.Begin();
+            }
+
+            MenuClosed = !MenuClosed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            //ButtonOpenMenu.Visibility = Visibility.Visible;
+            //ButtonCloseMenu.Visibility = Visibility.Collapsed;
         }
 
         private void listHome_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -54,7 +79,8 @@ namespace SmartMarket
 
         private void listCadastro_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-
+            //txTitle.Text = "Cadastro";
+            Frame.Source = new Uri("/Cadastro.xaml", UriKind.Relative);
         }
 
         private void listCaixa_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -65,6 +91,16 @@ namespace SmartMarket
         private void btnMenu_Click(object sender, RoutedEventArgs e)
         {
             //btnMenu.IsChecked
+        }
+
+        private void ButtonAlerta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
